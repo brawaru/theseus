b3785e4c4af165497c4ea1339032edd3fee4ba84
Sasha Sorokin <10401817+brawaru@users.noreply.github.com>
2025-01-31 16:34:07 +0700

    refactor(frontend): move onRequest out of options

--- apps/frontend/src/plugins/api/fetch.ts
+++ apps/frontend/src/plugins/api/fetch.ts
@@ -1,41 +1,52 @@
+import type { FetchContext } from "ofetch";
 import { type APIFetchOptions, type ResolvedAPIFetchOptions } from "./fetch-types.ts";
 import type { paths } from "./schema.d.ts";
 import { createTypedFetch } from "./typed-fetch/index.ts";
+
+function pushIntoArray<ItemType>(array: ItemType[], items?: ItemType | ItemType[]) {
+  if (items == null) return;
+  if (Array.isArray(items)) {
+    array.push(...items);
+  } else {
+    array.push(items);
+  }
+}
+
+function mergeHooks<HookType>(
+  initial: HookType | HookType[],
+  other?: HookType | HookType[],
+): HookType[] {
+  const hooks: HookType[] = [];
+  pushIntoArray(hooks, initial);
+  pushIntoArray(hooks, other);
+  return hooks;
+}
 
 export function setupAPIFetch(options?: APIFetchOptions) {
   const config = useRuntimeConfig();
 
   const baseURL = import.meta.server ? config.apiBaseUrl : config.public.apiBaseUrl;
 
-  const headers = Object.create(null);
+  async function onRequest({ options: _options }: FetchContext) {
+    const { headers, auth: _auth } = _options as ResolvedAPIFetchOptions;
+
+    if (import.meta.server) {
+      headers.set("x-ratelimit-key", config.rateLimitKey);
+    }
+
+    if (!headers.has("authorization") && _auth != null) {
+      const auth = typeof _auth === "function" ? _auth() : _auth;
+
+      if (auth != null) {
+        const token = await auth.getToken();
+        if (token != null) headers.set("Authorization", token);
+      }
+    }
+  }
 
   return createTypedFetch<paths>({
     baseURL,
-    headers,
     ...options,
-    onRequest: [
-      async function onRequest({ options: _options }) {
-        const { headers, auth: _auth } = _options as ResolvedAPIFetchOptions;
-
-        if (import.meta.server) {
-          headers.set("x-ratelimit-key", config.rateLimitKey);
-        }
-
-        if (!headers.has("authorization") && _auth != null) {
-          const auth = typeof _auth === "function" ? _auth() : _auth;
-
-          if (auth != null) {
-            const token = await auth.getToken();
-            if (token != null) headers.set("Authorization", token);
-          }
-        }
-      },
-
-      ...(Array.isArray(options?.onRequest)
-        ? options.onRequest
-        : options?.onRequest != null
-          ? [options.onRequest]
-          : []),
-    ],
+    onRequest: mergeHooks(onRequest, options?.onRequest),
   });
 }
